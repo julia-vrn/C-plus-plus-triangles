763e46e69ad4584b58d658e53d12bf7f4a143ad0
Julia Voronetskaja <julia.voronetksaja@gmail.com>
2018-09-08 12:38:37 +0300

    final

--- trinagles/ifexists.cpp
+++ trinagles/ifexists.cpp
@@ -1,20 +1,26 @@
 #include <QCoreApplication>
 #include "iostream"
 #include "area.cpp"
+#include "type.cpp"
 
 using namespace std;
 //функция, проверяющая, можно ли использовать переданные значения для построение треугольника
-//функция так же вызывает функцию triangleArea, для рассчета площади треугольника
-bool ifExists(float a, float b, float c){
+//функция так же вызывает функцию triangleArea(), для рассчета площади треугольника
+//и функцию type() для определения типа треугольника
+
+
+void ifExists(float a, float b, float c){
+    cout << "Enter the sides of your triangle: " << endl;
+    cin>>a;    cin>>b;    cin>>c;
 
     if((a+b>c)&&(a+c>b)&&(b+c>a)){
         cout << "Triangle!"<<endl;
         triangleArea(a, b, c);
-        return true;
+        type(a, b, c);
     }
     else {
-        cout<<"This is not a triangle!";
-        return false;
+        cout<<"This is not a triangle!"<<endl;
+
     }
 
 }
--- trinagles/main.cpp
+++ trinagles/main.cpp
@@ -1,7 +1,7 @@
 #include <QCoreApplication>
 #include "iostream"
 #include "ifexists.cpp"
-#include "type.cpp"
+//#include "type.cpp"
 
 
 using namespace std;
@@ -10,18 +10,8 @@ int main(int argc, char *argv[])
 {
     QCoreApplication app(argc, argv);
     float a = 0, b = 0, c = 0;
-
-    cout << "Enter the sides of your triangle: " << endl;
-    cin>>a;    cin>>b;    cin>>c;
-
-    if (ifExists(a, b, c)){
-        type(a, b, c);
-    }else {
-        cout << "This is not a triangle, try other parameters."<< endl;
+    while (true){
+        ifExists(a, b, c);
     }
-
-
-
-
     return app.exec();
 }
