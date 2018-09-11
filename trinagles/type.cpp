e909f92660df605ec45e361b798ce2130b7bb5cb
julia-vrn <julia.voronetskaja@gmail.com>
2018-09-11 21:09:47 +0300

    Update type.cpp

--- trinagles/type.cpp
+++ trinagles/type.cpp
@@ -7,16 +7,17 @@ using namespace std;
 //функция, использующая функция largest для определения наибольшего значения, которое передается функции typeOfTriangle
 //в качестве параметра, определяющего гипотенузу треугольника
 void type(float a, float b, float c){
-    float hypo = 0;
-    hypo = largest(a, b, c);
-    if((a==b)&&(a==c)){
+     if((a==b)&&(a==c)){
         typeOfTriangle(a, b, c);
-    }
-    else if(hypo==a){
-        typeOfTriangle(a, b, c);
-    }else if(hypo==b){
-        typeOfTriangle(b, a, c);
-    }else if(hypo = c){
-        typeOfTriangle(c, a, b);
+    }else{
+        float hypo = 0;
+        hypo = largest(a, b, c);
+        if(hypo==a){
+            typeOfTriangle(a, b, c);
+        }else if(hypo==b){
+            typeOfTriangle(b, a, c);
+        }else if(hypo = c){
+            typeOfTriangle(c, a, b);
+        }
     }
 }
