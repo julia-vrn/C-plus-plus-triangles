b0c41682060a66cf12c55e46fb14c372cba3af44
julia-vrn <julia.voronetskaja@gmail.com>
2018-09-12 09:53:57 +0300

    Update type.cpp

--- trinagles/type.cpp
+++ trinagles/type.cpp
@@ -7,9 +7,15 @@ using namespace std;
 //функция, использующая функция largest для определения наибольшего значения, которое передается функции typeOfTriangle
 //в качестве параметра, определяющего гипотенузу треугольника
 void type(float a, float b, float c){
-     if((a==b)&&(a==c)){
-        typeOfTriangle(a, b, c);
-    }else{
+     //если введены одинаковые значения
+    if(((a==b)&&(a==c))){
+        cout << "This is an equilateral triangle"<< endl; //равносторонний треугольник
+    }
+    //если введены значения, где два значения равны, а третье значение меньше
+    else if (((a<b)&&(b==c))||((b<a)&&(a==c))||((c<a)&&(a==b))) {
+        cout << "This is an acute isosceles triangle" <<endl; //остроугольный равнобедренный треугольник
+    }
+    else{
         float hypo = 0;
         hypo = largest(a, b, c);
         if(hypo==a){
