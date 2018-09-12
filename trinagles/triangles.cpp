0ce92cf2c46a74df19105b909e62227fa52a26ba
julia-vrn <julia.voronetskaja@gmail.com>
2018-09-12 09:55:17 +0300

    Update triangles.cpp

--- trinagles/triangles.cpp
+++ trinagles/triangles.cpp
@@ -5,7 +5,7 @@ using namespace std;
 //функция, определяющая тип треугольника, в качестве параметра принимает гипотенузу, катет 1, катет 2
 
 void typeOfTriangle(float hypo, float cath1, float cath2){
-    float sqhypo = hypo*hypo; // квадрат гипотенузы
+   float sqhypo = hypo*hypo; // квадрат гипотенузы
     float sqcathSum = cath1*cath1 + cath2*cath2; // квадрат катетов
 
     //прямоугольные треугольники
@@ -20,8 +20,6 @@ void typeOfTriangle(float hypo, float cath1, float cath2){
     else if(sqhypo<sqcathSum){
         if((hypo>cath1)&&(hypo>cath2)&&(cath1==cath2)){
             cout << "This is an acute isosceles triangle"<<endl;
-        }else if((hypo==cath1)&&(hypo==cath2)&&(cath1==cath2)){
-            cout << "This is an acute equilateral triangle";
         }else {
             cout << "This is an acute triangle."<<endl;
         }
